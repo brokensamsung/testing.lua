d5aec841cea0009eabce7ae82d432341ef5dbadb
brokensamsung <110091682+brokensamsung@users.noreply.github.com>
2023-12-23 23:41:24 +0100

    Update something.lua

--- something.lua
+++ something.lua
@@ -9,36 +9,40 @@ local maxAttempts = 10
 
 if player and character then
     local humanoid = character:WaitForChild("Humanoid")
-    local path = PathfindingService:CreatePath({
-        AgentRadius = 2,
-        AgentHeight = 5,
-        AgentCanJump = true,
-        AgentJumpHeight = 10,
-    })
+    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
-    local pathFound = false
-    for attempt = 1, maxAttempts do
-        path:ComputeAsync(character.Position, targetPosition)
-        if path.Status == Enum.PathStatus.Complete then
-            pathFound = true
-            break
+    if humanoidRootPart then
+        local path = PathfindingService:CreatePath({
+            AgentRadius = 2,
+            AgentHeight = 5,
+            AgentCanJump = true,
+            AgentJumpHeight = 10,
+        })
+
+        local pathFound = false
+        for attempt = 1, maxAttempts do
+            path:ComputeAsync(humanoidRootPart.Position, targetPosition)
+            if path.Status == Enum.PathStatus.Complete then
+                pathFound = true
+                break
+            end
+            wait(1)  -- Wait for a moment before the next attempt
         end
-        wait(1)  -- Wait for a moment before the next attempt
-    end
 
-    if not pathFound then
-        print("Pathfinding failed")
+        if not pathFound then
+            print("Pathfinding failed")
+        else
+            path.PathCompleted:Connect(function()
+                print("Pathfinding completed")
+            end)
+
+            path:MoveTo(targetPosition)
+
+            humanoid.MoveToFinished:Wait()
+        end
     else
-        path.PathCompleted:Connect(function()
-            print("Pathfinding completed")
-        end)
-
-        path:MoveTo(targetPosition)
-
-        humanoid.MoveToFinished:Wait()
+        warn("HumanoidRootPart not found in character.")
     end
 else
     warn("Player or character not found.")
 end
-
-
