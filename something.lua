abaa2b113b45b638fee10421cab94f9cad30b4c1
brokensamsung <110091682+brokensamsung@users.noreply.github.com>
2024-02-20 22:52:59 +0100

    Update something.lua

--- something.lua
+++ something.lua
@@ -1,25 +1,25 @@
-local TweenService = game:GetService("TweenService")
-local Players = game:GetService("Players")
+--local TweenService = game:GetService("TweenService")
+--local Players = game:GetService("Players")
 
-local player = Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+--local player = Players.LocalPlayer
+--local character = player.Character or player.CharacterAdded:Wait()
+--local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
-local targetPosition = Vector3.new(-902, 9, -93)
-local tweenInfo = TweenInfo.new(
-    2, -- Duration (in seconds)
-    Enum.EasingStyle.Linear, -- Easing style
-    Enum.EasingDirection.InOut, -- Easing direction
-    0, -- Number of times to repeat (0 for no repeat)
-    false, -- Whether to reverse the tween
-    0 -- Delay before tween starts (in seconds)
+--local targetPosition = Vector3.new(-902, 9, -93)
+--local tweenInfo = TweenInfo.new(
+    --2, -- Duration (in seconds)
+    --Enum.EasingStyle.Linear, -- Easing style
+    --Enum.EasingDirection.InOut, -- Easing direction
+    --0, -- Number of times to repeat (0 for no repeat)
+    --false, -- Whether to reverse the tween
+    --0 -- Delay before tween starts (in seconds)
 )
 
-local tween = TweenService:Create(humanoidRootPart, tweenInfo, {Position = targetPosition})
+--l--ocal tween = TweenService:Create(humanoidRootPart, tweenInfo, {Position = targetPosition})
 
-tween:Play()
+--tween:Play()
 
-wait(10)
+--w---ait(10)
 
 local TweenService = game:GetService("TweenService")
 local Players = game:GetService("Players")
