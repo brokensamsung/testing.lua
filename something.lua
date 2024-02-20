74a1781394f590fffb93eacad05e93baaff6f7a8
brokensamsung <110091682+brokensamsung@users.noreply.github.com>
2024-02-20 22:57:13 +0100

    Update something.lua

--- something.lua
+++ something.lua
@@ -1,4 +1,3 @@
-local TweenService = game:GetService("TweenService")
 local Players = game:GetService("Players")
 
 local player = Players.LocalPlayer
@@ -6,31 +5,9 @@ local character = player.Character or player.CharacterAdded:Wait()
 local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
 local targetPosition1 = Vector3.new(-902, 9, -93)
-local tweenInfo1 = TweenInfo.new(
-    2, -- Duration (in seconds)
-    Enum.EasingStyle.Linear, -- Easing style
-    Enum.EasingDirection.InOut, -- Easing direction
-    0, -- Number of times to repeat (0 for no repeat)
-    false, -- Whether to reverse the tween
-    0 -- Delay before tween starts (in seconds)
-)
-
-local tween1 = TweenService:Create(humanoidRootPart, tweenInfo1, {Position = targetPosition1})
-
-tween1:Play()
+humanoidRootPart.Position = targetPosition1
 
 wait(15)
 
 local targetPosition2 = Vector3.new(-794, -18, -1199)
-local tweenInfo2 = TweenInfo.new(
-    2, -- Duration (in seconds)
-    Enum.EasingStyle.Linear, -- Easing style
-    Enum.EasingDirection.InOut, -- Easing direction
-    0, -- Number of times to repeat (0 for no repeat)
-    false, -- Whether to reverse the tween
-    0 -- Delay before tween starts (in seconds)
-)
-
-local tween2 = TweenService:Create(humanoidRootPart, tweenInfo2, {Position = targetPosition2})
-
-tween2:Play()
+humanoidRootPart.Position = targetPosition2
