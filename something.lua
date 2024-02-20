450a8ce5af0f90e5bd2cbce664c05f0e358284b9
brokensamsung <110091682+brokensamsung@users.noreply.github.com>
2024-02-20 22:59:41 +0100

    Update something.lua

--- something.lua
+++ something.lua
@@ -7,7 +7,7 @@ local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 local targetPosition1 = Vector3.new(-902, 9, -93)
 humanoidRootPart.Position = targetPosition1
 
-wait(15)
+wait(5)
 
-local targetPosition2 = Vector3.new(-794, -18, -1199)
-humanoidRootPart.Position = targetPosition2
+--local targetPosition2 = Vector3.new(-794, -18, -1199)
+--humanoidRootPart.Position = targetPosition2
