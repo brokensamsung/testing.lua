c07c9ca9cecc982837f10697853f62290f972813
brokensamsung <110091682+brokensamsung@users.noreply.github.com>
2024-02-20 23:14:34 +0100

    Update something.lua

--- something.lua
+++ something.lua
@@ -1,13 +1,15 @@
-local Players = game:GetService("Players")
+--local Players = game:GetService("Players")
 
-local player = Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+--local player = Players.LocalPlayer
+--local character = player.Character or player.CharacterAdded:Wait()
+--local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
-local targetPosition1 = Vector3.new(-902, 9, -93)
-humanoidRootPart.Position = targetPosition1
+--local targetPosition1 = Vector3.new(-902, 9, -93)
+--humanoidRootPart.Position = targetPosition1
 
-wait(5)
+--wait(5)
 
 --local targetPosition2 = Vector3.new(-794, -18, -1199)
 --humanoidRootPart.Position = targetPosition2
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ShadowScripts4Roblox/PROTECTEDLUA/main/Scripts/APEIROPHOBIA/1.0.%3Blua'))() 
