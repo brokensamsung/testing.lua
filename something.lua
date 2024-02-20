2fd260ee3a200afdf668c706723ad25358e20745
brokensamsung <110091682+brokensamsung@users.noreply.github.com>
2024-02-20 22:54:46 +0100

    Update something.lua

--- something.lua
+++ something.lua
@@ -1,26 +1,3 @@
---local TweenService = game:GetService("TweenService")
---local Players = game:GetService("Players")
-
---local player = Players.LocalPlayer
---local character = player.Character or player.CharacterAdded:Wait()
---local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
-
---local targetPosition = Vector3.new(-902, 9, -93)
---local tweenInfo = TweenInfo.new(
-    --2, -- Duration (in seconds)
-    --Enum.EasingStyle.Linear, -- Easing style
-    --Enum.EasingDirection.InOut, -- Easing direction
-    --0, -- Number of times to repeat (0 for no repeat)
-    --false, -- Whether to reverse the tween
-    --0 -- Delay before tween starts (in seconds)
-)
-
---l--ocal tween = TweenService:Create(humanoidRootPart, tweenInfo, {Position = targetPosition})
-
---tween:Play()
-
---w---ait(10)
-
 local TweenService = game:GetService("TweenService")
 local Players = game:GetService("Players")
 
@@ -28,11 +5,8 @@ local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
--- Wait for 15 seconds
-wait(15)
-
-local targetPosition = Vector3.new(-794, -18, -1199)
-local tweenInfo = TweenInfo.new(
+local targetPosition1 = Vector3.new(-902, 9, -93)
+local tweenInfo1 = TweenInfo.new(
     2, -- Duration (in seconds)
     Enum.EasingStyle.Linear, -- Easing style
     Enum.EasingDirection.InOut, -- Easing direction
@@ -41,7 +15,22 @@ local tweenInfo = TweenInfo.new(
     0 -- Delay before tween starts (in seconds)
 )
 
-local tween = TweenService:Create(humanoidRootPart, tweenInfo, {Position = targetPosition})
+local tween1 = TweenService:Create(humanoidRootPart, tweenInfo1, {Position = targetPosition1})
 
-tween:Play()
+tween1:Play()
 
+wait(15)
+
+local targetPosition2 = Vector3.new(-794, -18, -1199)
+local tweenInfo2 = TweenInfo.new(
+    2, -- Duration (in seconds)
+    Enum.EasingStyle.Linear, -- Easing style
+    Enum.EasingDirection.InOut, -- Easing direction
+    0, -- Number of times to repeat (0 for no repeat)
+    false, -- Whether to reverse the tween
+    0 -- Delay before tween starts (in seconds)
+)
+
+local tween2 = TweenService:Create(humanoidRootPart, tweenInfo2, {Position = targetPosition2})
+
+tween2:Play()
